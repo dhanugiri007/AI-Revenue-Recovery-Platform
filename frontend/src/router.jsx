1e4705b48e98fe331a881149efcbed136d973d7b
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-08-29 14:34:07 +0530

    feat - added routes to show register and login page

---
+++ frontend/src/router.jsx
@@ -0,0 +1,15 @@
+import {createBrowserRouter} from 'react-router-dom';
+
+import Register from './features/auth/pages/Register.jsx';
+import Login from  './features/auth/pages/Login.jsx';
+
+export const router = createBrowserRouter([
+    {
+        path: '/login',
+        element : <Login/>
+    },
+    {
+        path : '/register',
+        element : <Register/>
+    }
+])
