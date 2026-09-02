71e56150b46d14333dbd0a9196eb17223e1a5092
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-09-02 07:27:49 +0530

    fix: changed the payment model stages

--- backend/src/models/payment.model.js
+++ backend/src/models/payment.model.js
@@ -18,7 +18,7 @@ const paymentSchema = new mongoose.Schema({
     },
     status : {
         type : String,
-        enum : ["pending","success","recovering","escalated"],
+        enum : ["pending","success","failed"],
         required: true
     },
     failureReason : {
