c421bc045b5a7559e38e4218d64bf602bdac70f9
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-09-03 22:57:48 +0530

    fix: changed uri name to VITE_BACKEND_URI

--- frontend/src/features/auth/service/auth.api.js
+++ frontend/src/features/auth/service/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URI,
+    baseURL: import.meta.env.VITE_BACKEND_URI,
     withCredentials: true
 })
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.BACKEND_URI, {
+export const socket = io(import.meta.env.VITE_BACKEND_URI, {
     withCredentials: true,
     autoConnect: true
 });
