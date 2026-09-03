ad3ec670879885ddd202b6266243f9ba2c4344a7
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-09-03 22:35:00 +0530

    feat : added the socket io frontend client also backend live url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3000', {
+export const socket = io(import.meta.env.BACKEND_URI, {
     withCredentials: true,
     autoConnect: true
 });
